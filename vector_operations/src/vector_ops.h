be6b93c1cbd023ba79c07c6f448ff077e89e5279
Sergey Revkov <mail4sergey@mail.ru>
2020-10-03 18:44:05 +0300

    added vector_ops.h hw prog_1

--- vector_operations/src/vector_ops.h
+++ vector_operations/src/vector_ops.h
@@ -2,10 +2,106 @@
 #include <vector>
 #include <iostream>
 
+namespace task{	
+	const double EPSILON = 1e-7;
 
-namespace task {
+	std::vector<double> operator+ (const std::vector<double> &a, const std::vector<double> &b){
+		std::vector<double> c;
+		for (size_t i = 0; i < a.size(); ++i){
+			c.push_back(a[i] + b[i]);
+		}
+		return c;
+	}
+	
+	std::vector<double> operator+ (const std::vector<double> &a){
+		return a;
+	}
 
-// Your code here...
+	std::vector<double> operator- (const std::vector<double> &a){
+		std::vector<double> b;
+		for (size_t i = 0; i < a.size(); ++i){
+			b.push_back(-a[i]);
+		}
+		return b;
+	}
 
+	std::vector<double> operator- (const std::vector<double> &a, const std::vector<double> &b){
+		std::vector<double> c;
+		for (size_t i = 0; i < a.size(); ++i){
+			c.push_back(a[i] - b[i]);
+		}
+		return c;
+	}
 
-}  // namespace task
+	double operator* (const std::vector<double> &a, const std::vector<double> &b){
+		double sum = 0;
+		for (size_t i = 0; i < a.size(); ++i){
+			sum += a[i] * b[i];
+		}
+		return sum;
+	}
+
+	std::vector<double> operator% (const std::vector<double> &a, const std::vector<double> &b){
+		std::vector<double> c;
+		c.push_back(a[1] * b[2] - a[2] * b[1]);
+		c.push_back(a[2] * b[0] - a[0] * b[2]);
+		c.push_back(a[0] * b[1] - a[1] * b[0]);
+		return c;
+	}
+
+	std::ostream &operator<< (std::ostream &os, const std::vector<double> &a){
+		for (size_t i = 0; i < a.size(); ++i) os << a[i] << " ";
+		os << '\n';
+		return os;
+	}
+
+	std::istream &operator>> (std::istream &is, std::vector<double> &a){
+		size_t n = 0;
+		is >> n;
+		a.resize(n);
+		double x;
+		for (size_t i = 0; i < n; ++i){
+			is >> x;
+			a[i] = x;
+		}
+		return is;
+	}
+
+	bool operator|| (const std::vector<double> &a, const std::vector<double> &b){
+		double d1 = 0., d2 = 0.;
+		for (size_t i = 0; i < a.size(); ++i){
+			d1 += a[i] * a[i];
+			d2 += b[i] * b[i];
+		}
+		d1 = sqrt(d1);
+		d2 = sqrt(d2);		
+		if ((1. - (a * b) / (d1 * d2) < EPSILON) || (1. + (a * b) / (d1 * d2) < EPSILON))
+		       	return true;
+		return false;
+	}	
+	
+	bool operator&& (const std::vector<double> &a, const std::vector<double> &b){
+		return (a * b > 0) && (a || b);
+	}
+
+	void reverse (std::vector<double> &a){
+		size_t n = a.size() / 2;
+		for (size_t i = 0; i < n; ++i) std::swap(a[i], a[a.size() - 1 - i]);
+	}
+	
+	std::vector<int> operator| (const std::vector<int> &a, const std::vector<int> &b){
+		std::vector<int> c;
+		for (size_t i = 0; i < a.size(); ++i){
+			c.push_back(a[i] | b[i]);
+		}
+		return c;	
+	}	
+
+	std::vector<int> operator& (const std::vector<int> &a, const std::vector<int> &b){
+		std::vector<int> c;
+		for (size_t i = 0; i < a.size(); ++i){
+			c.push_back(a[i] & b[i]);
+		}
+		return c;	
+	}	
+}  
