efe7a27cda622d112bebb6d32d66363ee2cfa2ba
Sergey Revkov <mail4sergey@mail.ru>
2020-10-05 13:24:04 +0300

    add hw3

--- simple_compose/src/main.cpp
+++ simple_compose/src/main.cpp
@@ -6,7 +6,9 @@ typedef std::function<int (int)> Op;
 
 
 Op compose (size_t n, Op ops[]) {
-    /// Your code goes here.
+    if (n == 0) return [] (int x) -> int { return x; };
+    else if (n == 1) return [ops] (int x) -> int { return ops[0](x); };
+    else return [=] (int x) { return compose(n - 1, ops)(ops[n - 1](x)); };
 }
 
 
